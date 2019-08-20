b2f8e677a84f513dc772617dd602bc2ca83f6b8a
hdelanoe <27762445+hdelanoe@users.noreply.github.com>
2019-08-20 16:35:45 +0200

    Env.hpp devWin

--- src/Env.hpp
+++ src/Env.hpp
@@ -8,30 +8,50 @@ class Env
 	public:
 
 		Env(void);
-		~Env(void);
+		~Env(void) {};
 
-		GLFWwindow*	getGLFWwin(void);
-		float const	&getTime(void) const	{ return time; }
-		void		setTime(float t)	{ time = t; }
+		void				initGlfwEnvironment(void);
+		void				initGlfwWindow(void);
+		void				initGladEnvironment(void);
+		void				initCL(void);
+
+		GLFWwindow*			getGLFWwin(void);
+		float const			&getTime(void) const	{ return time; }
+		void				setTime(float t)		{ time = t; }
+		
+		bool				isCLExtensionSupported(const char* extension);
 
 	private:
 
-		GLFWwindow*	window;
-		int 		width;
-		int 		height;
+		GLFWwindow*			window;
+		
+		size_t 				GlobalWorkSize[3];
+		size_t  			LocalWorkSize[3];
 
-		float		time;
+		GLuint				hPobj;
+		GLuint				hCobj;
+		struct xyzw*		hvel;
+		cl_mem				dPobj;
+		cl_mem				dCobj;
+		cl_mem				dVel;
 
-		void		initGlfwEnvironment(void);
-		void		initGlfwWindow(void);
-		void		initGladEnvironment(void);
+		cl_command_queue	CmdQueue;
+		cl_device_id		Device;
+		cl_kernel			Kernel;
+		cl_platform_id		Platform;
+		cl_program			Program;
 
-        class 		GlfwException: public std::exception { virtual const char* what() const throw(); };
-        class 		WindowException: public std::exception { virtual const char* what() const throw(); };
-        class       GladException: public std::exception { virtual const char* what() const throw(); };
+
+		float				time;
+		float				frames;
+
+        class 				GlfwException: public std::exception { virtual const char* what() const throw(); };
+        class 				WindowException: public std::exception { virtual const char* what() const throw(); };
+        class      			GladException: public std::exception { virtual const char* what() const throw(); };
+        class      			CLException: public std::exception { virtual const char* what() const throw(); };
 
 };
 
-void				framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void						framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 #endif
